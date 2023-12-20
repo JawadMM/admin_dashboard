54e164566484638619ad6b2ad703a2f21c3f8e38
JawadMM <Almuttawa.j@hotmail.com>
2023-12-20 09:55:20 +0300

    setting up themes and colors

---
+++ client/src/App.js
@@ -0,0 +1,25 @@
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { useSelector } from "react-redux";
+import { themeSettings } from "theme";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+function App() {
+  const mode = useSelector((state) => state.global.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route element={<Layout />} />
+            
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
